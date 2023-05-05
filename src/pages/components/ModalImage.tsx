3f04f85e54950e6ab22f7a1c373746223ebdfec6
kenvilar <kenvilar@gmail.com>
2023-05-05 15:44:09 +0800

    initial layout for image

---
+++ src/pages/components/ModalImage.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Gizeh from "../../images/43693.jpg";
+
+const ModalImage = () => {
+  return (
+    <>
+      <div>
+        <Image
+          src={Gizeh}
+          alt="Gizeh"
+          className="mx-auto duration-200 w-60 hover:scale-105"
+        />
+      </div>
+    </>
+  );
+};
+
+export default ModalImage;
